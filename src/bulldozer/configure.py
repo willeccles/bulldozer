df801aeb42bd030fb69d2ef33fd981d62dcb7767
Will Eccles <will@eccles.dev>
2021-10-07 20:29:03 -0400

    updates to project loading

--- src/bulldozer/configure.py
+++ src/bulldozer/configure.py
@@ -1,8 +1,19 @@
 import os
 import sys
 import importlib
+import importlib.util
+
+# TODO move this to a more accessible location for other source files
+def LoadProject(project_root: str):
+    spec = importlib.util.spec_from_file_location('',
+            os.path.join(project_root, 'dozer.py'))
+    # TODO where to put this so that __pycache__ isn't everywhere
+    spec.cached = None
+    project = importlib.util.module_from_spec(spec)
+    #sys.modules['project'] = project
+    spec.loader.exec_module(project)
+    return project
 
 def Configure(args):
-    sys.path.append(args.path)
-    project = importlib.import_module('dozer')
+    project = LoadProject(args.path)
     print(project.CoolFunction())
--- test/helloworld/dozer.py
+++ test/helloworld/dozer.py
@@ -1,2 +1,2 @@
 def CoolFunction():
-    print("Hello Bulldozer")
+    return "Hello Bulldozer"
